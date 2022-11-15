2d8f5fa2a4e40a9514d81aaa8467158f3198ad7b
armentc <armentanocarolina@gmail.com>
2022-11-15 19:36:01 -0300

    Alterando as funções.

--- projetos/calculadora/app.py
+++
@@ -1,5 +0,0 @@
-def nomeia(nome):
-    print(f"Nome={nome}")
-
-def idade(idade):
-    print(f"Idade={idade}")
--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -1,11 +1,58 @@
 def soma(a, b):
-    return a+b
+    """
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a + b -> (int, float)
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a + b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
+
 
 def subtracao(a, b):
-    return a-b
+    """
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a - b -> (int, float)
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a - b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
+
 
 def divisao(a, b):
-    return a/b
+    """
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a / b -> (int, float)
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        if b == 0:
+            print('Divisão invalida, recebido 0 como denominador')
+            return 0
+        else:
+            return a / b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
 
-def multiplicacao(a, b)
-    return a*b
+
+def multiplicacao(a, b):
+    """
+    args:
+        a: (int, float)
+        b: (int, float)
+    return:
+        a * b -> (int, float)
+    """
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a * b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser um número, recebido {a} ({type(a)}), b ({type(b)})")
